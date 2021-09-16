fa2686636c6a5394a531d4d3d6b75b01b9a7fb71
SBTHDR <subrata.halder13d@gmail.com>
2021-09-16 22:09:40 +0600

    Category Model

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return 'Backend Api response successful';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
