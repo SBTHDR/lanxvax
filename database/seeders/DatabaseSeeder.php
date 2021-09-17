9a6ca1bc53290ef8ebf08b0557034a25115a8e0a
SBTHDR <subrata.halder13d@gmail.com>
2021-09-17 20:14:27 +0600

    People Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\People;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        People::factory(30)->create();
 
         $user = new User();
         $user->name = 'Snow';
